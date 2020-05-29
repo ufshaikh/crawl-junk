bd04dcf33aa5544bb1effc371c363d62c16ed851
Umer Shaikh <umer.f.shaikh@gmail.com>
2020-05-29 08:30:53 -0500

    Remove spell miscast calculator
    Recent commits have revealed max miscast damage in-game and made the
    probability distribution of damage much simpler. I considered updating
    the calculator to match but I doubt I would use it much. I think it can
    simply be removed.

--- initfile/make.sh
+++ initfile/make.sh
@@ -3,7 +3,7 @@
 rm init.txt
 touch init.txt
 echo -e "-- -*- mode: lua; -*-\n\n{\n\ndebugp = true -- debug printing\n" >> init.txt
-for f in utility_functions.lua spells.lua resting_and_exploring.lua \
+for f in utility_functions.lua resting_and_exploring.lua \
 conditional_options.lua ready.lua; do cat $f; echo; done >> init.txt
 echo -e "}\n" >> init.txt
 cat options.txt >> init.txt
